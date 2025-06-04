428a07aa749c7618ad975f4ff1528d1a380cbb9d
Micro9261 <140260920+Micro9261@users.noreply.github.com>
2025-06-04 17:01:06 +0200

    Initial tested tree commit

---
+++ src/main/java/com/EnderLite/app/App.java
@@ -0,0 +1,12 @@
+package com.EnderLite.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Stage stage = new Stage();
+        stage.show();
+    }
+}
