fc58d4eb3efa1b633b48fd55db14aeea38386cb8
Shaman <theshaman29@gmail.com>
2025-06-04 17:18:21 +0200

    egewg
    wegewg

--- src/main/java/com/EnderLite/app/App.java
+++ src/main/java/com/EnderLite/app/App.java
@@ -3,10 +3,11 @@ package com.EnderLite.app;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class App extends Application{
+public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Stage stage = new Stage();
         stage.show();
+        System.out.println("Hi");
     }
 }
