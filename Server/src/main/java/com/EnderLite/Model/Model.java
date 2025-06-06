0107ac806ccbf12cfd537672e2961b21cec4ed01
Shaman <theshaman29@gmail.com>
2025-06-06 16:23:14 +0200

    refactoring db and attempt to implement model

--- Server/src/main/java/com/EnderLite/Model/Model.java
+++ Server/src/main/java/com/EnderLite/Model/Model.java
@@ -1,22 +1,48 @@
 package com.EnderLite.Model;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import org.json.JSONObject;
+import com.owlike.genson.Genson;
 
 public class Model {
     public Model() {
 
     }
 
-    public User getUser() {
-        return new User();
+    public User getUser(UUID uuid) {
+        // File userFile = new File("./");
+        InputStream in = getClass().getResourceAsStream("/Users/" + uuid.toString() + ".json");
+
+        String json = new BufferedReader(
+                new InputStreamReader(in, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+
+        Map<String, Object> map = (new Genson()).deserialize(json, Map.class);
+
+        // Object map2 = genson.deserialize(json, Object.class);
+
+        // JSONObject data = new JSONObject(JSONData);
+        // User user = new User(uuid, data.getString("login"), data.getString("email"),
+        // data.getString("passwordHash"));
+        User user = new User();
+        user.ID = uuid;
+        user.Login = map.get("login").toString();
+        user.Email = map.get("email").toString();
+        user.PasswordHash = map.get("passwordHash").toString();
+
+        return user;
     }
 
     public Chat getChat() {
         return new Chat();
-    }
-
-    public void getUserDBData() {
-        System.out.println("Reading db");
     }
 
 }
--- Server/src/main/java/com/EnderLite/app/App.java
+++ Server/src/main/java/com/EnderLite/app/App.java
@@ -1,13 +1,18 @@
 package com.EnderLite.app;
 
 import com.EnderLite.Model.*;
+import java.util.UUID;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Server is booting up...");
 
         Model model = new Model();
-        model.getUserDBData();
+
+        User user = model.getUser(UUID.fromString("f7c3de3d-1fea-4d7c-a8b0-29f63c4c3454"));
+
+        System.out.println(user.Login + " " + user.Email + " " + user.PasswordHash + " " + user.ID.toString());
+
     }
 }
 
