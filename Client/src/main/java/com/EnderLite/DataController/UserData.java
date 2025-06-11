9853de4783aed5db8d2a8d5de424e9bbac99c62c
Micro9261 <140260920+Micro9261@users.noreply.github.com>
2025-06-11 14:36:07 +0200

    UserData finished

--- Client/src/main/java/com/EnderLite/DataController/UserData.java
+++ Client/src/main/java/com/EnderLite/DataController/UserData.java
@@ -1,13 +1,77 @@
 package com.EnderLite.DataController;
 
-import java.util.List;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+
+enum Rank{
+    USER,
+    ADMIN
+}
+
+record ChatData(String chatId, Rank rand) {
+    //Default
+}
 
 
 public class UserData {
 
-    private String login;
-    private String email;
-    private List<String> friendsList;
-    private List<String> chatsList;
+    private StringProperty login;
+    private StringProperty email;
+    private ListProperty<String> friendsList = new SimpleListProperty<String>();
+    private ListProperty<ChatData> chatsList = new SimpleListProperty<ChatData>();
     
+    //login
+    public StringProperty getLoginProperty(){
+        return login;
+    }
+
+    public void setLogin(String newLogin){
+        login.set(newLogin);
+    }
+
+    public String getLogin(){
+        return login.get();
+    }
+
+    //email
+    public StringProperty getEmailProperty(){
+        return email;
+    }
+
+    public void setEmail(String newEmail){
+        email.set(newEmail);
+    }
+
+    public String getEmail(){
+        return email.get();
+    }
+
+    //friendsList
+    public ListProperty<String> getFriendsListProperty(){
+        return friendsList;
+    }
+
+    public void addFriend(String login){
+        friendsList.add(login);
+    }
+
+    public ObservableList<String> getFriendsList(){
+        return friendsList.get();
+    }
+
+    //chatList
+    public ListProperty<ChatData> getChatListProperty(){
+        return chatsList;
+    }
+
+    public void addChat(String chatId, Rank rank){
+        chatsList.add(new ChatData(chatId, rank));
+    }
+
+    public ObservableList<ChatData> getChatList(){
+        return chatsList.get();
+    }
+
 }
